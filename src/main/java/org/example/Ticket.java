236d6ad107aef75c6d6c25960bea5b3b13c1b09e
TofuStore <humormy@gmail.com>
2022-07-15 09:55:53 -0700

    TicketCanDocumentCarLicensePlate passing

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -2,12 +2,18 @@ package org.example;
 
 public class Ticket {
     private double timeEntered;
-    public Ticket(double timeEntered) {
+    private String carLicensePlate;
+    public Ticket(double timeEntered, Car car) {
         this.timeEntered = timeEntered;
+        this.carLicensePlate = car.getLicensePlate();
     }
 
     public double getTimeEntered() {
         return timeEntered;
+    }
+
+    public String getCarLicensePlate() {
+        return carLicensePlate;
     }
 }
 
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,14 +1,21 @@
+import org.example.Car;
 import org.example.Ticket;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicketTest {
     @Test
     public void TicketCanDocumentTimeEntered() {
-        Ticket ticket = new Ticket(8.5);
+        Car car = new Car("123", "black");
+        Ticket ticket = new Ticket(8.5, car);
         assertEquals(8.5, ticket.getTimeEntered());
+    }
+
+    @Test
+    public void TicketCanDocumentCarLicensePlate() {
+        Car car = new Car("123", "black");
+        Ticket ticket = new Ticket(8.5, car);
+        assertEquals("123", ticket.getCarLicensePlate());
     }
 }
